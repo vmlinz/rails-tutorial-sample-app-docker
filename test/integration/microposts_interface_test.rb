106b1ef97a9a0f874f9a38913461e12918b5ddff
Zaicheng Qi <vmlinz@gmail.com>
2016-01-16 22:56:34 +0800

    Finish exercise 11

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -12,6 +12,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
 
     assert_no_difference "Micropost.count" do
       post microposts_path, micropost: { content: "" }
@@ -20,9 +21,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
 
     content = "The test post"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference "Micropost.count", 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
+    assert assigns(:micropost).picture?
 
     assert_redirected_to root_url
     follow_redirect!
@@ -36,5 +39,19 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "34 microposts", response.body
+
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match '0 micropost', response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match '1 micropost', response.body
   end
 end
